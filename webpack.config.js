6f34cbf7468198c311adb7f55d9bc850d688a4e8
Adrian Moraret <adrian.moraret@yahoo.com>
2016-10-26 20:40:58 -0700

    bundle.js creation included in the npm scripts

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require("path")
+module.exports = {
+    entry: [
+      path.resolve(__dirname, 'src/index')
+    ],
+    output: {
+      path: __dirname + '/dist/scripts',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+        {test: /(\.css)$/, loaders: ['style', 'css']},
+        {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+        {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+        {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+      ]
+    }
+}
