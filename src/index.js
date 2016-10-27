cd9d7ef0ec52f9df8c29036630b27b0f4309dab3
Adrian Moraret <adrian.moraret@yahoo.com>
2016-10-26 23:55:54 -0700

    Added bootstrap and font awesome

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/glyphicons-halflings/css/glyphicons-halflings.css';
+import 'font-awesome-webpack';
 console.log('hi there!');
--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,9 @@ module.exports = {
         {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
         {test: /(\.css)$/, loaders: ['style', 'css']},
         {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-        {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-        {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+        {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=imagxe/svg+xml'}
       ]
     }
 }
